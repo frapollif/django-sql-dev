dd11e94ee2c2d23f66586b23e96ce906ac922d99
Fulvio Frapolli <fulvio.frapolli@edu.ti.ch>
2024-10-28 12:08:13 +0100

    network and non collectstatic

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
- # 'ENGINE': 'mysql.connector.django',
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'yourdb',
-#         'USER': 'root',
-#         'PASSWORD': 'password',
-#         'HOST':'localhost',
-#         'PORT':'3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+ # 'ENGINE': 'mysql.connector.django',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yourdb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST':'mysql',
+        'PORT':'3306',
+    }
+}
 
 
 
